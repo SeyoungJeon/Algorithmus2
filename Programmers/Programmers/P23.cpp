731ff2bd8a7eca95bed3916aa6a44af01a4f4737
SeyoungJeon <34870901+SeyoungJeon@users.noreply.github.com>
2020-03-28 02:08:02 +0900

    프로그래머스 탑  (스택/큐) 풀이 참고

---
+++ Programmers/Programmers/P23.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+stack<pair<int,int>> t;
+
+vector<int> solution(vector<int> heights) {
+	vector<int> answer;
+
+	for (int i = 0; i < heights.size(); i++) {
+		while (!t.empty()) {
+			if (t.top().first > heights[i]){
+				answer.push_back(t.top().second);
+				break;
+			}
+			t.pop();
+		}
+
+		if (t.empty()) {
+			answer.push_back(0);
+		}
+
+		t.push({ heights[i],i + 1 });
+	}
+
+	return answer;
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	solution({ 6,9,5,7,4 });
+
+	return 0;
+}
